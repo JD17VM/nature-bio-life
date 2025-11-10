910c0fccaca29292e6f86b945d177abdd3bf0188
JD17VM <jdiegodiego4@gmail.com>
2025-11-10 09:43:08 -0500

    feat: :sparkles: implement tipoMaterial model entity

---
+++ app/Models/TipoMaterial.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class TipoMaterial extends Model
+{
+    use HasFactory;
+
+    /**
+     * Los atributos que se pueden asignar masivamente.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'nombre',
+        'extension_permitida',
+    ];
+
+    /**
+     * Obtiene los materiales asociados con este tipo.
+     */
+    public function materiales(): HasMany
+    {
+        return $this->hasMany(Material::class);
+    }
+}
